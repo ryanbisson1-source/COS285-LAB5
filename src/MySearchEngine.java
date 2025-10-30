b444fd6e8255b706a84d6dfbdb672f1d4623d022
ryanbisson1-source <ryan.bisson1@maine.edu>
2025-10-30 17:09:22 -0400

    CONSTRUCTOR

---
+++ src/MySearchEngine.java
@@ -0,0 +1,14 @@
+public class MySearchEngine {
+
+    public MySearchEngine(){
+        String title = this.title;
+        int votes = this.votes;
+        String releaseDate = this.releaseDate;
+        int runTime = this.runTime;
+        int budget = this.budget;
+        String genre = this.genre;
+        String overview = this.overview;
+
+    }
+    
+}
